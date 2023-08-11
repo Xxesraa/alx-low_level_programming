be667436c0fece741f79f326b76740d287c8dc09
Xxesraa <esraaeldie999@gmail.com>
2023-08-11 06:30:31 -0700

    l

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Description: print all aplhabet letters
+ *
+ * Return:Alawys 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
