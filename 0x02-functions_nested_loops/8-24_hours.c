32a1871c3135be319fee0173ffe45fc608dbed4c
Xxesraa <esraaeldie999@gmail.com>
2023-08-15 12:15:33 -0700

    k

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print every minuti of the day
+*/
+
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (j = 0; j <= 23; j++)
+	{
+		for (i = 0; i <= 59; i++)
+		{
+			_putchar((j / 10) + 48);
+			_putchar((j % 10) + 48);
+			_putchar(':');
+			_putchar((i / 10) + 48);
+			_putchar((i % 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
+
