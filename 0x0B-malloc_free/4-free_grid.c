e99b036474f27326f8d6f1c37140d9a4687843fc
Xxesraa <esraaeldie999@gmail.com>
2023-09-04 13:34:08 -0700

    E

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -4,7 +4,7 @@
  * free_grid - rees a 2 dimensional grid previously
  *  created by your alloc_grid function
  * @grid: grid of memories
- * height:int
+ * @height:int
  * Return: void
 */
 void free_grid(int **grid, int height)
