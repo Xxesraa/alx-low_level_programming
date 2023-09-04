79f45b9ec7586f7e8c9653de72ec803dd9fe8680
Xxesraa <esraaeldie999@gmail.com>
2023-09-04 10:00:25 -0700

    y

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * @argv: pointer to in array of command line argumant
  * Return: 0-success, non-zero-fail.
 */
-int main(int argc, char argv[] __attribute__((unused)))
+int main(int argc, char argv[]__attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
