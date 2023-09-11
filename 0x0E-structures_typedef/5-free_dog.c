3761e6009ed43f461467b66ca6c24fc979bdbeea
Xxesraa <esraaeldie999@gmail.com>
2023-09-11 16:33:08 -0700

    E

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog -  a function that frees dogs
+ * @d: yo dawg
+ *
+ * Return: void
+*/
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->name)
+			free(d->name);
+
+		if (d->owner)
+			free(d->owner);
+				free(d);
+	}
+}
