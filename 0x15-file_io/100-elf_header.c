553a5a268f5547e7bf5d380b2bc41dc02462d76d
Xxesraa <esraaeldie999@gmail.com>
2023-10-02 16:43:52 -0700

    l

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -223,3 +223,45 @@ void print_entry(Elf64_Ehdr h)
 	}
 		printf("\n");
 }
+/**
+ * main - program
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 1 on success 0 on failure
+*/
+int main(int ac, char **av)
+{
+	int f;
+	Elf64_Ehdr h;
+	ssize_t b;
+
+	if (ac != 2)
+		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n"), exit(98);
+	f = open(av[1], O_RDONLY);
+	if (f == -1)
+		dprintf(STDERR_FILENO, "Cant't open file: %s\n", av[1]), exit(98);
+	b = read(f, &h, sizeof(h));
+	if (b < 1 || b != sizeof(h))
+		dprintf(STDERR_FILENO, "Can't read from file: %s\n", av[1]), exit(98);
+	if (h.e_ident[0] == 0x7f && h.e_ident[1] == 'E' && h.e_ident[2] == 'L' &&
+			h.e_ident[3] == 'F')
+	{
+		printf("ELF Header:\n");
+	}
+	else
+		dprintf(STDERR_FILENO, "Not ELF file: %s\n", av[1]), exit(98);
+
+	print_magic(h);
+	print_class(h);
+	print_data(h);
+	print_Version(h);
+	print_osabi(h);
+	print_osabi_more(h);
+	print_abiversion(h);
+	print_type(h);
+	print_entry(h);
+	if (close(f))
+		dprintf(STDERR_FILENO, "Error closing file descriptor: %d\n", f), exit(98);
+	return (EXIT_SUCCESS);
+}
