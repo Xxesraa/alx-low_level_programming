fbf479d4e76e668b324ccf57874e428d55082399
Xxesraa <esraaeldie999@gmail.com>
2023-10-02 13:27:44 -0700

    l

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,11 +1,5 @@
 #include "main.h"
 
-#define USAGE "Usage: cp file_from file_to\n"
-#define ERR_NOREAD "Error: Can't read from file NAME_OF_THE_FILE %s\n"
-#define ERR_NOWRITE "Error: Can't write to NAME_OF_THE_FILE %s\n"
-#define ERR_NOCLOSE "Error: Can't close fd FD_VALUE %d\n"
-#define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
-
 /**
  * main - program
  * @ac: argument count
@@ -16,32 +10,14 @@
 
 int main(int ac, char **av)
 {
-	int ff = 0, tf = 0;
-	ssize_t b;
-	char buf[READ_BUF_SIZE];
+	int i;
 
 	if (ac != 3)
-		dprintf(STDERR_FILENO, USAGE), exit(97);
-	ff = open(av[1], O_RDONLY);
-
-	if (ff == -1)
-		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
-	tf = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, PERMISSIONS);
-
-	if (tf == -1)
-		dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
-
-	while ((b = read(ff, buf, READ_BUF_SIZE)) > 0)
-		if (write(tf, buf, b) != b)
-			dprintf(STDERR_FILENO, ERR_NOWRITE, av[2]), exit(99);
-	if (b == -1)
-		dprintf(STDERR_FILENO, ERR_NOREAD, av[1]), exit(98);
-
-	ff = close(ff);
-	tf = close(tf);
-	if (ff)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, ff), exit(100);
-	if (tf)
-		dprintf(STDERR_FILENO, ERR_NOCLOSE, tf), exit(100);
-	return (EXIT_SUCCESS);
+	{
+		dprintf(2, "Usage: %s file_from file_to\n", av[0]);
+		exit(1);
+	}
+	i = append_text_to_file(av[1], av[2]);
+	printf("-> %i)\n", i);
+	return (0);
 }
