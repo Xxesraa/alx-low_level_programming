cee8eabc88c84731d15db68f8c23203fa4ec5429
Xxesraa <esraaeldie999@gmail.com>
2023-12-18 10:42:21 -0800

    l

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,31 +7,32 @@
  * @letters: The number of letters to read and print.
  *
  * Return: The actual number of letters it could read and print.
- *If the file can not be opened or read, return 0.
- *If filename is NULL, return 0.
- *If write fails or does not write the expected amount of bytes, return 0.
+ *         If the file can not be opened or read, return 0.
+ *         If filename is NULL, return 0.
+ *         If write fails or does not write the expected amount
+ *         of bytes, return 0.
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file_descriptor, read_result, write_result;
+	int file_descriptor;
+	ssize_t read_result, write_result;
 	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
-	file_descriptor = open(filename, O_RDONLY);
+	file_descriptor = popen(filename, O_RDONLY);
 	if (file_descriptor == -1)
 		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
-		close(file_descriptor);
+		pclose(file_descriptor);
 		return (0);
 	}
 
-	read_result = read(file_descriptor, buffer, letters);
+	read_result = fread(file_descriptor, buffer, letters);
 	if (read_result == -1)
 	{
 		free(buffer);
@@ -39,8 +40,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	write_result = write(STDOUT_FILENO, buffer, read_result);
-	if (write_result == -1 || write_result != read_result)
+	write_result = fwrite(STDOUT_FILENO, buffer, read_result);
+	if (write_result == -1 || (size_t)write_result != letters)
 	{
 		free(buffer);
 		close(file_descriptor);
@@ -52,4 +53,3 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	return (read_result);
 }
-
